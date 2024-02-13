41cd9c53ea8e8530e82a040695423ef518782831
myusername <myemail>
2024-02-13 17:08:10 +0530

    backend url added

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080",
-  // baseURL: "https://stack-overflow-backend-gwva.onrender.com",
+  // baseURL: "http://localhost:8080",
+  baseURL: "https://stack-overflow-backend-gwva.onrender.com",
 });
 
 API.interceptors.request.use((req) => {
